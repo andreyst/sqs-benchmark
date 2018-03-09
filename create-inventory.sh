4b44aeeba609a7d6c2f32e441f4b9bc2bec912f4
Andrey Stolbovsky <andrey.stolbovsky@gmail.com>
2018-03-09 12:29:04 +0300

    Add ansible user to create inventory script

--- create-inventory.sh
+++ create-inventory.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "[sqs-benchmark]" > hosts
-aws ec2 describe-instances --filter Name=tag:Purpose,Values=SqsBenchmark --filter Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].PublicIpAddress' --output json | python -c "import sys;import json; buf = sys.stdin.read(); data = json.loads(buf); print '\n'.join([ ip for ip_list in data for ip in ip_list ])" >> hosts
+aws ec2 describe-instances --filter Name=tag:Purpose,Values=SqsBenchmark --filter Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].PublicIpAddress' --output json | python -c "import sys;import json; buf = sys.stdin.read(); data = json.loads(buf); print('\n'.join([ '%s ansible_user=ubuntu' % ip for ip_list in data for ip in ip_list ]))" >> hosts
 echo -n "New inventory size: "
 grep -v "sqs-benchmark" hosts | wc -l
